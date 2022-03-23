76bc56f042876fff0316844d9f283220602b6e5b
Cseplonorbert <cseplo.norbert.95@gmail.com>
2022-03-24 00:50:19 +0100

    Implement CartController.js/refreshCart method.

--- src/main/webapp/static/js/controller/CartController.js
+++ src/main/webapp/static/js/controller/CartController.js
@@ -1,12 +1,20 @@
+import {dataHandler} from "../data/dataHandler.js";
+import {domManager} from "../view/domManager.js";
 
 export let CartController = {
     container: new Map(),
-    modal: null,
+    productList: null,
     cartItem: null,
+    numberOfItems: 0,
+    numberOfItemsTag: null,
+    totalPriceTag: null,
     initCart: function () {
         initAddToCartButtons();
-        CartController.modal = document.getElementById('modal');
+        CartController.productList = document.getElementById('productList');
         CartController.cartItem = document.getElementById("cartItem");
+        CartController.numberOfItemsTag = document.getElementById("numberOfItems");
+        CartController.numberOfItemsTag.textContent = CartController.numberOfItems + " items";
+        CartController.totalPriceTag = document.getElementById("totalPrice");
     },
     updateAddToCartButtons: function() {
         initAddToCartButtons();
@@ -14,18 +22,17 @@ export let CartController = {
     addToCart: function (productId) {
         if (CartController.container.has(productId)) {
             CartController.container.set(productId, CartController.container.get(productId) + 1);
-            addNewItemToCart(productId);
+            CartController.numberOfItems ++;
+            refreshCart();
         } else {
             CartController.container.set(productId, 1);
+            CartController.numberOfItems ++;
             refreshCart();
         }
     },
     removeProduct: function (productId) {
 
     },
-    updateContent: function () {
-
-    }
 }
 
 function initAddToCartButtons() {
@@ -38,10 +45,34 @@ function addProductToCart(clickEvent) {
     CartController.addToCart(productId);
 }
 
-function addNewItemToCart(productId) {
-
+function refreshCart() {
+    dataHandler.getCart(Object.fromEntries(CartController.container)).then((response) => {
+        domManager.clearElement(CartController.productList);
+        response.items.forEach((item) => createProductCardInCart(item));
+        CartController.totalPriceTag.textContent = response.total + " BTC";
+    }).catch((error) => {
+        alert(error);
+    });
+    CartController.numberOfItemsTag.textContent = CartController.numberOfItems + " items";
 }
 
-function refreshCart() {
+function createProductCardInCart(item) {
+    const cardDiv = CartController.cartItem.content.cloneNode(true);
 
+    const image = cardDiv.querySelector(".product-image");
+    image.setAttribute("src", `/static/img/product_${item.id}.png`);
+
+    const itemName = cardDiv.querySelector(".product-name");
+    itemName.textContent = item.name;
+
+    const itemSupplier = cardDiv.querySelector(".product-supplier");
+    itemSupplier.textContent = item.supplier;
+
+    const itemQuantity = cardDiv.querySelector(".product-quantity");
+    itemQuantity.textContent = item.quantity;
+
+    const price = cardDiv.querySelector(".product-price");
+    price.textContent = item.SumPrice + " BTC";
+
+    CartController.productList.appendChild(cardDiv);
 }
