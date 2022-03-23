e00f726e5e47305642afba6e3e33758d842e9a14
Cseplonorbert <cseplo.norbert.95@gmail.com>
2022-03-23 22:41:31 +0100

    Implement dataHandler.js/apiPost function.

--- src/main/webapp/static/js/data/dataHandler.js
+++ src/main/webapp/static/js/data/dataHandler.js
@@ -16,3 +16,16 @@ async function apiGet(url) {
     }
     return response.json();
 }
+
+async function apiPost(url, payload) {
+        let response = await fetch(url, {
+            method: "POST",
+            headers: {"content-type":"application/json"},
+            body:JSON.stringify(payload)
+
+        });
+        if(!response.ok) {
+            throw Error(`${response.status} ${response.statusText}`);
+        }
+        return response.json();
+}
