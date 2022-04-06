81260e380c17ba33e89869e7cbba117121c27591
Nitram137 <nemethmartin99@gmail.com>
2022-04-06 10:09:24 +0200

    Extend init_db with bundles

--- src/main/sql/init_db.sql
+++ src/main/sql/init_db.sql
@@ -24,6 +24,22 @@ CREATE TABLE public.categories (
     department text NOT NULL
 );
 
+CREATE TABLE public.bundles (
+    id serial NOT NULL PRIMARY KEY,
+    name text NOT NULL,
+    description text NOT NULL
+);
+
+CREATE TABLE public.products_bundles (
+    id serial NOT NULL PRIMARY KEY,
+    bundle_id integer NOT NULL,
+    product_id integer NOT NULL
+);
+
 ALTER TABLE ONLY public.products
     ADD CONSTRAINT fk_supplier_id FOREIGN KEY (supplier_id) REFERENCES public.suppliers(id),
     ADD CONSTRAINT fk_category_id FOREIGN KEY (category_id) REFERENCES public.categories(id);
+
+ALTER TABLE ONLY public.products_bundles
+    ADD CONSTRAINT fk_bundle_id FOREIGN KEY (bundle_id) REFERENCES public.bundles(id),
+    ADD CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES public.products(id);
