43934870d0381bafbdb86053437e9e0d22db83ea
Nitram137 <nemethmartin99@gmail.com>
2022-04-06 11:42:21 +0200

    Add data access object java database connection classes

---
+++ src/main/java/com/codecool/shop/dao/db_implementation/BundleDaoJdbc.java
@@ -0,0 +1,28 @@
+package com.codecool.shop.dao.db_implementation;
+
+import com.codecool.shop.dao.BundleDao;
+import com.codecool.shop.model.Bundle;
+
+import java.util.List;
+
+public class BundleDaoJdbc implements BundleDao {
+    @Override
+    public void add(Bundle bundle) {
+
+    }
+
+    @Override
+    public Bundle find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<Bundle> getAll() {
+        return null;
+    }
+}
