f63392e45ba5263788a148c35f93c13eb5c8187e
Cseplonorbert <cseplo.norbert.95@gmail.com>
2022-04-06 23:11:14 +0200

    feat: ProductJSON/ add categoryId and supplierId.

--- src/main/java/com/codecool/shop/model/ProductJSON.java
+++ src/main/java/com/codecool/shop/model/ProductJSON.java
@@ -7,13 +7,19 @@ public class ProductJSON {
     private final String price;
     private final String description;
     private final String supplier;
+    private final String category;
+    private final int categoryId;
+    private final int supplierId;
 
-    public ProductJSON(int id, String name, String price, String description, String supplier) {
+    public ProductJSON(int id, String name, String price, String description, String supplier, String category, int categoryId, int supplierId) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.description = description;
         this.supplier = supplier;
+        this.category = category;
+        this.categoryId = categoryId;
+        this.supplierId = supplierId;
     }
 
 }
